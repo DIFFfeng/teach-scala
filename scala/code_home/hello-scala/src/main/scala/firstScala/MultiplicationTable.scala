60bfe2849f3067f6f52a46a1794b05586ce73cf3
AlphaWan <3043777973@qq.com>
2025-07-10 20:27:18 +0800

    Scala基础教学-firstDay

--- scala/code_home/hello-scala/src/main/scala/firstScala/MultiplicationTable.scala
+++ scala/code_home/hello-scala/src/main/scala/firstScala/MultiplicationTable.scala
@@ -2,11 +2,8 @@ package firstScala
 
 object MultiplicationTable {
   def main(args: Array[String]): Unit = {
-    for (i <- 1 to 9) {
-      for (j <- 1 to i) {
-        print(s"$j × $i = ${i * j}\t")
-      }
-      println()
+    for(bianliang <- 1 to 9) {
+      println(bianliang) // line
     }
   }
 }
